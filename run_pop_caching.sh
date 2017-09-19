4405f90e41f57710e0ea11c4404204fc1b678ff7
Andrew Chupakhin <atom1543@yandex.ru>
2017-09-19 07:03:10 +0000

    Fixed start script for pop_caching.

--- run_pop_caching.sh
+++ run_pop_caching.sh
@@ -5,24 +5,32 @@ DATASET_PATH="/home/soc/ARCCN/dataset2_pid_5_time_cid_size.log"
 SIZES="1000000000 4493148160 6740246528 8987344896 13481541632 22469935104"
 
 MIN_LEARN_LIMIT=40000
-MAX_LEARN_LIMIT=400000
+MAX_LEARN_LIMIT=160000
 LEARN_LIMIT_STEP=40000
 RE_ESTIMATION_PERIODS="10000"
+
+RE_ESTIMATION_PERIODS_MIN="2000"
+RE_ESTIMATION_PERIODS_MAX="20000"
+RE_ESTIMATION_PERIODS_STEP="2000"
 
 declare -a pids
 
 
-for period in $RE_ESTIMATION_PERIODS
+for period in $(seq $RE_ESTIMATION_PERIODS_MIN $RE_ESTIMATION_PERIODS_STEP $RE_ESTIMATION_PERIODS_MAX)
 do
 	for learn_limit in $(seq $MIN_LEARN_LIMIT $LEARN_LIMIT_STEP $MAX_LEARN_LIMIT)
 	do
+<<<<<<< Updated upstream
 		# mkdir "/home/soc/ARCCN/results_pop_caching/l_"$learn_limit"_re_"$period
+=======
+		mkdir "/home/soc/ARCCN/results_pop_caching/l_"$learn_limit"_re_"$period
+>>>>>>> Stashed changes
 		DIR="/home/soc/ARCCN/results_pop_caching/l_"$learn_limit"_re_"$period
 		i=0
 		for size in $SIZES
 		do
 			echo "$ALGO_PATH pop_caching $size $learn_limit $period $DATASET_PATH > $DIR/$size".txt""
-			# $ALGO_PATH pop_caching $size $learn_limit $period $DATASET_PATH > $DIR/$size".txt" &
+			$ALGO_PATH pop_caching $size $learn_limit $period $DATASET_PATH > $DIR/$size".txt" &
 			pids[${i}]=$!
 			let "i+=1"
 		done
